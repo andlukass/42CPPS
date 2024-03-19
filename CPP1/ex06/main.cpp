291af7690b4969f3b02644ed6129577309d0fa0b
andlukass <lopess.andrade@gmail.com>
2024-03-19 14:18:12 +0000

    fix arguments in ex06

--- CPP1/ex06/main.cpp
+++ CPP1/ex06/main.cpp
@@ -2,8 +2,10 @@
 #include <iostream>
 
 int main(int argc, char *argv[]){
-	if (argc != 2)
+	if (argc != 2){
+		std::cout << "wrong arguments" << std::endl;
 		return (1);
+	}
 
 	Harl harl;
 	harl.complain(argv[1]);
