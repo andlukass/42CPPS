78d9a9e340cfa44738e9510302383fe1fb8a1540
andlukass <lopess.andrade@gmail.com>
2024-04-12 20:49:13 +0100

    change name

--- CPP2/ex00/Fixed.cpp
+++ CPP2/ex00/Fixed.cpp
@@ -2,31 +2,29 @@
 
 Fixed::Fixed() {
 	std::cout << "Default constructor called" << std::endl;
-	this->decimal = 0;
+	this->_raw = 0;
 }
 
 Fixed::~Fixed() {
 	std::cout << "Destructor called" << std::endl;
-	this->decimal = 0;
 }
 
 Fixed::Fixed( const Fixed &toCopy ) {
 	std::cout << "Copy constructor called" << std::endl;
-	this->decimal = toCopy.decimal;
-
+	this->_raw = toCopy.getRawBits();
 }
 
 Fixed& Fixed::operator=(const Fixed &other) {
 	std::cout << "Copy assignment operator called" << std::endl;
-	this->decimal = other.getRawBits();
+	this->_raw = other.getRawBits();
 	return *this;
 }
 
 int Fixed::getRawBits( void ) const {
 	std::cout << "getRawBits member function called" << std::endl;
-	return this->decimal;
+	return this->_raw;
 }
 
 void Fixed::setRawBits( int const raw ) {
-	this->decimal = raw;
+	this->_raw = raw;
 }
--- CPP2/ex00/Fixed.hpp
+++ CPP2/ex00/Fixed.hpp
@@ -5,14 +5,14 @@
 
 class Fixed {
 	private:
-		int decimal;
-		static const int bits = 8;
+		int _raw;
+		static const int _bits = 8;
 
 	public:
 		Fixed();
 		~Fixed();
 		Fixed( const Fixed &toCopy );
-		Fixed& operator=(const Fixed &other);
+		Fixed& operator=( const Fixed &other );
 		
 		int getRawBits( void ) const;
 		void setRawBits( int const raw );
