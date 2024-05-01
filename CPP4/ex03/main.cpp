2d5f7a25848156d2e5accb7bfbef66e9a49291f7
andlukass <lopess.andrade@gmail.com>
2024-05-01 14:37:22 +0100

    add cure class

--- CPP4/ex03/main.cpp
+++ CPP4/ex03/main.cpp
@@ -1,6 +1,8 @@
 #include "Ice.hpp"
+#include "Cure.hpp"
 #include "Character.hpp"
 #include "MateriaSource.hpp"
+
 #include <vector>
 
 int main(){
@@ -8,15 +10,15 @@ int main(){
 	{
 		IMateriaSource* src = new MateriaSource();
 		src->learnMateria(new Ice());
-		// src->learnMateria(new Cure());
+		src->learnMateria(new Cure());
 
 		ICharacter* me = new Character("me");
 
 		AMateria* tmp;
 		tmp = src->createMateria("ice");
 		me->equip(tmp);
-		// tmp = src->createMateria("cure");
-		// me->equip(tmp);
+		tmp = src->createMateria("cure");
+		me->equip(tmp);
 
 		ICharacter* bob = new Character("bob");
 
